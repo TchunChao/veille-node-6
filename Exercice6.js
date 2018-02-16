276d852d01af8ed3dde1d5f46e2d5b2fce4fbdd6
Tchun Chao <chaot_xp@live.fr>
2018-02-16 15:00:14 -0500

    Progression exercice

---
+++ Exercice6.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+app.use(express.static('public'));
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+/* on associe le moteur de vue au module «ejs» */
+app.set('view engine', 'ejs'); // générateur de template
+
+let db // variable qui contiendra le lien sur la BD
+MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
+ if (err) return console.log(err)
+ db = database.db('carnet_adresse')
+// lancement du serveur Express sur le port 8081
+ app.listen(8081, () => {
+ console.log('connexion à la BD et on écoute sur le port 8081')
+ })
+})
+
+app.get('/adresse', (req, res) => {
+ console.log('la route route get / = ' + req.url)
+ 
+ var cursor = db.collection('adresse')
+                .find().toArray(function(err, resultat){
+ if (err) return console.log(err)
+ // transfert du contenu vers la vue index.ejs (renders)
+ // affiche le contenu de la BD
+ res.render('adresse.ejs', {adresse: resultat})
+ }) 
+})
+
+app.post('/ajouter', (req, res) => {
+ db.collection('adresse').save(req.body, (err, result) => {
+ if (err) return console.log(err)
+ console.log('sauvegarder dans la BD')
+ res.redirect('/adresse')
+ })
+})
